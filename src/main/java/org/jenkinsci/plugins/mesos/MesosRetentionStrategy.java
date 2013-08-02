57793fa61085ec65ab6358649cb76e3f9c863f3e
Chris Aniszczyk <zx@twitter.com>
2013-08-02 13:29:30 -0500

    Cleanup license headers
    Signed-off-by: Chris Aniszczyk <zx@twitter.com>

--- src/main/java/org/jenkinsci/plugins/mesos/MesosRetentionStrategy.java
+++ src/main/java/org/jenkinsci/plugins/mesos/MesosRetentionStrategy.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2013 Twitter, Inc. and other contributors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jenkinsci.plugins.mesos;
 
 import static hudson.util.TimeUnit2.MINUTES;
