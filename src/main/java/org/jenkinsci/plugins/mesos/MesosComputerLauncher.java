9bf1b5fe56d478f87fb369d587940c050f533149
Fabian Donze <fdonze@gmail.com>
2016-02-23 17:30:51 -0500

    Wait for slave to come online to avoid provisioning of additional slaves.

--- src/main/java/org/jenkinsci/plugins/mesos/MesosComputerLauncher.java
+++ src/main/java/org/jenkinsci/plugins/mesos/MesosComputerLauncher.java
@@ -107,10 +107,25 @@ public class MesosComputerLauncher extends ComputerLauncher {
       // Since we just launched a slave, remove it from pending deletion in case it was marked previously while
       // we were waiting for resources to be available
       computer.getNode().setPendingDelete(false);
+      waitForSlaveConnection(computer);
       logger.println("Successfully launched slave" + name);
     }
 
     LOGGER.info("Finished launching slave computer " + name);
+  }
+
+  private void waitForSlaveConnection(MesosComputer computer) {
+    while (computer.isOffline() && computer.isConnecting()) {
+      try {
+        LOGGER.info("Waiting for slave computer connection " + name);
+        Thread.sleep(5000);
+      } catch (InterruptedException ignored) {}
+    }
+    if (computer.isOnline()) {
+      LOGGER.info("Slave computer connected " + name);
+    } else {
+      LOGGER.warning("Slave computer offline " + name);
+    }
   }
 
   /**
