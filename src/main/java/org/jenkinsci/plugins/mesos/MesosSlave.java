5216d7ecef0bc8923ff510aec6659e2c7e7611cb
Ben Wood <ben@benclarkwood.com>
2018-04-30 11:00:51 -0700

    Enhance metrics (#14)
    * Track per agent offer metrics
    
    * Add better timing metrics
    
    * Adding logging to verify actual timeframe of executor launch

--- src/main/java/org/jenkinsci/plugins/mesos/MesosSlave.java
+++ src/main/java/org/jenkinsci/plugins/mesos/MesosSlave.java
@@ -24,6 +24,8 @@ import hudson.slaves.ComputerLauncher;
 import jenkins.model.Jenkins;
 
 import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -42,7 +44,11 @@ public class MesosSlave extends Slave {
   private final double cpus;
   private final int mem;
   private final double diskNeeded;
-  private transient final Timer.Context provisioningContext;
+  private final UUID uuid = UUID.randomUUID();
+  private transient final Timer.Context provisionToReady;
+  private transient final Timer.Context provisionToMesos;
+  private transient final Timer mesosToReady;
+  private transient long mesosHandoffTime;
 
 
   private boolean pendingDelete;
@@ -50,7 +56,13 @@ public class MesosSlave extends Slave {
   private static final Logger LOGGER = Logger.getLogger(MesosSlave.class
       .getName());
 
-  public MesosSlave(MesosCloud cloud, String name, int numExecutors, MesosSlaveInfo slaveInfo, Timer.Context provisioningContext) throws IOException, FormException {
+  public MesosSlave(MesosCloud cloud,
+                    String name,
+                    int numExecutors,
+                    MesosSlaveInfo slaveInfo,
+                    Timer.Context provisionToReadyContext,
+                    Timer.Context provisionToMesosContext,
+                    Timer mesosToReady) throws IOException, FormException {
     super(name,
           slaveInfo.getLabelString(), // node description.
           StringUtils.isBlank(slaveInfo.getRemoteFSRoot()) ? "jenkins" : slaveInfo.getRemoteFSRoot().trim(),   // remoteFS.
@@ -67,7 +79,9 @@ public class MesosSlave extends Slave {
     this.cpus = slaveInfo.getSlaveCpus() + (numExecutors * slaveInfo.getExecutorCpus());
     this.mem = slaveInfo.getSlaveMem() + (numExecutors * slaveInfo.getExecutorMem());
     this.diskNeeded = slaveInfo.getdiskNeeded();
-    this.provisioningContext = provisioningContext;
+    this.provisionToReady = provisionToReadyContext;
+    this.provisionToMesos = provisionToMesosContext;
+    this.mesosToReady = mesosToReady;
     LOGGER.fine("Constructing Mesos slave " + name + " from cloud " + cloud.getDescription());
   }
 
@@ -107,8 +121,18 @@ public class MesosSlave extends Slave {
     return idleTerminationMinutes;
   }
 
-  public Timer.Context getProvisioningContext() {
-    return provisioningContext;
+  public void provisionedAndReady() {
+    mesosToReady.update(System.currentTimeMillis() - mesosHandoffTime, TimeUnit.MILLISECONDS);
+    provisionToReady.stop();
+  }
+
+  public void provisionedToMesos() {
+    provisionToMesos.stop();
+    mesosHandoffTime = System.currentTimeMillis();
+  }
+
+  public UUID getUuid() {
+    return uuid;
   }
 
   public void terminate() {
