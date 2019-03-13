3e5feab84652d10bc3c7b8051dbd5c5bd078ef61
Karsten Jeschkies <k@jeschkies.xyz>
2019-03-13 18:30:00 +0100

    Depend on USI via source control.  (#22)
    Summary:
    Until USI is released we declare it as a source controlled dependency.
    This allows us to introduce changes and use them right away in this
    project.
    
    Depends on #21 and mesosphere/usi#69.

--- src/main/java/org/jenkinsci/plugins/mesos/MesosApi.java
+++ src/main/java/org/jenkinsci/plugins/mesos/MesosApi.java
@@ -1,5 +1,7 @@
 package org.jenkinsci.plugins.mesos;
 
+import com.mesosphere.usi.core.models.PodSpec;
+import com.mesosphere.mesos.client.MesosClient;
 import org.apache.commons.lang.NotImplementedException;
 
 import java.util.concurrent.CompletableFuture;
