c4e6879586c87d59deea7d4eb44fb98f72ddf4a7
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-10-30 15:00:00 +0100

    Run full test suite.

--- ci/run.sh
+++ ci/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+systemctl stop mesos-master
+systemctl stop mesos-agent
+
 # Java
 yum install -y wget
 wget -q https://d3pxv6yz143wms.cloudfront.net/8.232.09.1/java-1.8.0-amazon-corretto-devel-1.8.0_232.b09-1.x86_64.rpm
 yum localinstall -y java-1.8.0-amazon-corretto-devel-1.8.0_232.b09-1.x86_64.rpm
 
-# JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto ./gradlew check  --info
-JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto ./gradlew test --tests "*DockerAgentTest" --info
+JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto ./gradlew check  --info
 
 
