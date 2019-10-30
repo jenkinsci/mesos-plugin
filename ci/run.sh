f26e2d1ffb4ef3141dbe8ffa5f6b78b93efc89c4
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-10-30 15:33:34 +0100

    Clean before build.

--- ci/run.sh
+++ ci/run.sh
@@ -9,6 +9,6 @@ yum install -y wget
 wget -q https://d3pxv6yz143wms.cloudfront.net/8.232.09.1/java-1.8.0-amazon-corretto-devel-1.8.0_232.b09-1.x86_64.rpm
 yum localinstall -y java-1.8.0-amazon-corretto-devel-1.8.0_232.b09-1.x86_64.rpm
 
-JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto ./gradlew check  --info
+JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto ./gradlew clean check  --info
 
 
