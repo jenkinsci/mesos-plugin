1ed3f60d00cbc2073b056f68fd4903b5f08579f9
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-11-27 14:56:13 +0100

    Correct javadoc.

--- src/main/java/org/jenkinsci/plugins/mesos/config/models/faultdomain/DomainFilterModel.java
+++ src/main/java/org/jenkinsci/plugins/mesos/config/models/faultdomain/DomainFilterModel.java
@@ -7,7 +7,9 @@ import hudson.model.AbstractDescribableImpl;
  * A simple config model that enables a hetero descriptor list. See {@link StringDomainFilter},
  * {@link Any}, {@link Home} and the usage in MesosAgentSpecTemplate/config.jelly for usage.
  *
- * @see https://www.previous.cloudbees.com/blog/introducing-variability-jenkins-plugins
+ * @see <a
+ *     href="https://www.previous.cloudbees.com/blog/introducing-variability-jenkins-plugins">Introducing
+ *     Variability into Jenkins Plugins</a>
  */
 public abstract class DomainFilterModel extends AbstractDescribableImpl<DomainFilterModel> {
   public abstract DomainFilter getFilter();
