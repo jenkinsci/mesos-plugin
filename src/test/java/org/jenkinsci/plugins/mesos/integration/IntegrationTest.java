de4c1a7caeebbb9af90b8cc505abb1df861a74b5
Karsten Jeschkies <k@jeschkies.xyz>
2020-05-20 12:27:58 +0200

    Timeout integration test. (#403)

--- src/test/java/org/jenkinsci/plugins/mesos/integration/IntegrationTest.java
+++ src/test/java/org/jenkinsci/plugins/mesos/integration/IntegrationTest.java
@@ -4,9 +4,19 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Timeout;
 
+/**
+ * Tag integration tests and apply timeout to <i>each</i> test method.
+ *
+ * <p>The timeout can be overridden individually.
+ *
+ * @see org.junit.jupiter.api.Timeout
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Tag("integration")
+@Timeout(value = 3, unit = TimeUnit.MINUTES)
 public @interface IntegrationTest {}
